16cc2bbc7f1a21684f8415667b46c235a62f8e0f
muditag007 <muditag007@gmail.com>
2023-08-17 01:18:18 +0530

    ft: mongo error

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connection = mongoose
-  .createConnection('mongodb://127.0.0.1:27017/taskManager')
+  .createConnection('mongodb://0.0.0.0:27017/taskManager')
   .on('open', () => {
     console.log('MongoDB Connected')
   })
